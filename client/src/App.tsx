3914b559734169d652e48d52fa9b132944453777
Rabindra Joshi <mail@rjoshi.net>
2020-04-29 04:35:07 +0545

    SB-01: ESLint: remove unused import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/auth/login";
 import Dashboard from "./components/dashboard/dashboard";
