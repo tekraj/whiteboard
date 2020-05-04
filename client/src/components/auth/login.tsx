5eed1dfd4c33a672a6cc918997403fde434eb186
jyotindra subedi <jyotindra@proshore.eu>
2020-05-04 22:57:17 +0545

    new client id added

--- client/src/components/auth/login.tsx
+++ client/src/components/auth/login.tsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { GoogleLogin } from "react-google-login";
 import { Redirect } from "react-router-dom";
 
-export class Login extends Component {
+export class Login extends Component<any> {
   state = {
     data: [],
     name: "",
@@ -11,34 +11,41 @@ export class Login extends Component {
     token: null,
   };
 
-
   componentDidMount() {
     this.getTestQuery();
-  };
+  }
 
   async getTestQuery() {
     const API_URL = process.env.REACT_APP_API_URL;
     const API_BASE_SLUG = process.env.REACT_APP_API_BASE_SLUG;
 
-    const API_BASE = `${API_URL}/${API_BASE_SLUG}`
+    const API_BASE = `${API_URL}/${API_BASE_SLUG}`;
 
     fetch(`${API_BASE}/test_query`)
-      .then(data => {
-        return data.json()
+      .then((data) => {
+        return data.json();
       })
-      .then(res => {
-        this.setState({ data: res })
-      })
-  };
-  GOOGLE_CLIENT_ID:string = process.env.REACT_APP_GOOGLE_CLIENT_ID!;
-  responseGoogle = (response: object) => {
+      .then((res) => {
+        this.setState({ data: res });
+      });
+  }
+  GOOGLE_CLIENT_ID: string = process.env.REACT_APP_GOOGLE_CLIENT_ID!;
+  signup(profileObj: any) {
+    console.log(profileObj);
+    this.props.history.push("/dashboard");
+  }
+  responseGoogle = (response: any) => {
     console.log("this is response", response);
-    return <Redirect to="/dashboard" />;
+    this.signup(response.profileObj);
   };
   render() {
-    const data = this.state.data.length ? this.state.data.map((item: any) =>
-      <p key={item.id}>{item.id}. {item.firstname} {item.lastname}</p>
-    ) : null
+    const data = this.state.data.length
+      ? this.state.data.map((item: any) => (
+          <p key={item.id}>
+            {item.id}. {item.firstname} {item.lastname}
+          </p>
+        ))
+      : null;
     return (
       <React.Fragment>
         <GoogleLogin
@@ -52,18 +59,14 @@ export class Login extends Component {
             return (
               <div>
                 <h1>From database:</h1>
-                <ol>
-                  {data}
-                </ol>
+                <ol>{data}</ol>
               </div>
-            )
-          }
-          else {
-            return <p>No Data!</p>
+            );
+          } else {
+            return <p>No Data!</p>;
           }
         })()}
-      </React.Fragment >
-
+      </React.Fragment>
     );
   }
 }
